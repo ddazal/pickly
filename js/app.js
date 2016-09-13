be4b8e03ebacd99106baac68e33c6acc41c005c5
David Daza <03dazal@gmail.com>
2016-09-13 09:23:36 -0500

    ngView y Font Awesome

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-var app = angular.module('appPickly', []);
+var app = angular.module('appPickly', ['ngRoute']);
+
+app.config(['$routeProvider',function($routeProvider) {
+	$routeProvider
+		.when('/p16f877a',{
+			templateUrl : 'partials/p16f877a.html'
+		})
+		.when('/p18f4550',{
+			templateUrl : 'partials/p18f4550.html'
+		});
+}]);
--- js/controladores/app-controller.js
+++ js/controladores/app-controller.js
@@ -3,11 +3,11 @@
 		$scope.pics = [
 			{
 				name: 'PIC16F877A',
-				url: 'p16f877a.html'
+				url: '#p16f877a'
 			},
 			{
 				name: 'PIC18F4550',
-				url: 'p18f4550.html'
+				url: '#p18f4550'
 			}
 		];
 	});	
