c9fbbc292a17892ae10f59b1df5341bfadf5c1c6
David Daza <03dazal@gmail.com>
2017-03-03 11:27:20 -0500

    Actualización README.md y cambio de puerto

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 var router = require('./routes')
 
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 3000
 
 app.use(express.static('public'))
 app.use('/', router)
